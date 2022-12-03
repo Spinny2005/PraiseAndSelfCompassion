070dd1a6744420f4c589c0ed3bf9ba0ef7d6ad15
Spinny2005 <spencer_boggs@icloud.com>
2022-12-02 22:23:17 -0800

    Nice Things

---
+++ praiseandselfcompassion.py
@@ -0,0 +1,38 @@
+import math
+import random
+
+print("")
+print("     Welcome to praise and")
+print("        Self Compassion!")
+print("    Created by Spencer Boggs")
+print("")
+input("    Press [Enter] to Start")
+while True:
+    print("")
+    name = input("Please enter your name: ")
+    print("")
+    
+    insults = [
+        name + ", you are a pitiful excuse for a human being", 
+        "Gek", 
+        "Protozoan", 
+        "More of your conversation would infect my brain.", 
+        "You, minion, are too saucy.", 
+        "I do wish thou were a dog, that I might love thee something."]
+
+    if (name.casefold() == "no"):
+        print("Liar. Thats not your name")
+    elif (name):
+        print("  Hello, " + name)
+        for x in insults:
+            rand = random.randint(0,len(insults)-1)
+            print("")
+            print("  " + insults[rand])
+            insults.pop(rand)
+            print("")
+            input("  Press [Enter] for next compliment")
+        print("")
+        print("  You aren't even worth any more words.")
+    else:
+        print("Fuck you. You little bitch.")
+        print("Enter your name now!")
